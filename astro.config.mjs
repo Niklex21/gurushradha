8d0b47d3613dcc086fb23dadb84cea1ea6a108ce
Alex Nikanov <alekseynikanov.21@gmail.com>
2024-10-05 15:06:27 -0700

    Installed prettier, ran the formatter, and basic storyblok page setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,33 +1,33 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-import react from '@astrojs/react';
-import { loadEnv } from 'vite';
-import storyblok from '@storyblok/astro';
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import react from "@astrojs/react";
+import { loadEnv } from "vite";
+import storyblok from "@storyblok/astro";
 
-import vercel from '@astrojs/vercel/serverless';
+import vercel from "@astrojs/vercel/serverless";
 
-const env = loadEnv('', process.cwd(), 'STORYBLOK');
+const env = loadEnv("", process.cwd(), "STORYBLOK");
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
-      tailwind({
-          applyBaseStyles: false,
-      }),
-      react(),
-      storyblok({
-          accessToken: env.STORYBLOK_TOKEN,
-          components: {
-              // Add your components here
-          },
-          apiOptions: {
-              // Choose your Storyblok space region
-              region: 'us', // optional,  or 'eu' (default)
-          },
-      }),
+    tailwind({
+      applyBaseStyles: false,
+    }),
+    react(),
+    storyblok({
+      accessToken: env.STORYBLOK_TOKEN,
+      components: {
+        page_home: "storyblok/pages/Home",
+      },
+      apiOptions: {
+        // Choose your Storyblok space region
+        region: "us", // optional,  or 'eu' (default)
+      },
+    }),
   ],
 
-  output: 'server',
+  output: "server",
   adapter: vercel(),
 });
