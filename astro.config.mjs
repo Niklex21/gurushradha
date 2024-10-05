d6a8b487fc44d27b521f25d80bbe3453e50ec30c
Alex Nikanov <alekseynikanov.21@gmail.com>
2024-10-05 13:54:44 -0700

    Init setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,33 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+import react from '@astrojs/react';
+import { loadEnv } from 'vite';
+import storyblok from '@storyblok/astro';
+
+import vercel from '@astrojs/vercel/serverless';
+
+const env = loadEnv('', process.cwd(), 'STORYBLOK');
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [
+      tailwind({
+          applyBaseStyles: false,
+      }),
+      react(),
+      storyblok({
+          accessToken: env.STORYBLOK_TOKEN,
+          components: {
+              // Add your components here
+          },
+          apiOptions: {
+              // Choose your Storyblok space region
+              region: 'us', // optional,  or 'eu' (default)
+          },
+      }),
+  ],
+
+  output: 'server',
+  adapter: vercel(),
+});
