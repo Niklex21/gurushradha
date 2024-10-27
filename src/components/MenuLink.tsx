b0b0f69be3c0860f101162616d0dc1dab60dde42
Alex Nikanov <alekseynikanov.21@gmail.com>
2024-10-27 02:11:18 -0400

    Adding a gallery (#3)
    * updated astro
    
    * wip gallery
    
    * Adding a gallery

--- src/components/MenuLink.tsx
+++ src/components/MenuLink.tsx
@@ -19,8 +19,8 @@ export function MenuLink({ item, pathname }: { item: MenuItemStoryblok, pathname
             </a>
             {(item.submenus && item.submenus.length > 0) && (
                 <div className="absolute top-0 left-0 flex-col p-2 mt-6 gap-2 -ml-2 rounded-xl bg-white hidden group-hover/menu:flex">
-                    {item.submenus.map((submenu) => (
-                        <MenuLink item={submenu} pathname={pathname} />
+                    {item.submenus.map((submenu, index) => (
+                        <MenuLink item={submenu} pathname={pathname} key={index} />
                     ))}
                 </div>
             )}
