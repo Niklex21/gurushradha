c190ba2ccd2e884273b5d3cbf6b31fc923953129
Alex Nikanov <alekseynikanov.21@gmail.com>
2024-11-11 22:12:55 -0800

    Removed a bunch of console.logs

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -7,9 +7,6 @@ export const server = {
       const INSTAGRAM_ACCESS_KEY = import.meta.env.INSTAGRAM_ACCESS_KEY;
       const INSTAGRAM_APP_ID = import.meta.env.INSTAGRAM_APP_ID;
 
-      console.log(import.meta.env.INSTAGRAM_ACCESS_KEY);
-      console.log(import.meta.env.INSTAGRAM_APP_ID);
-
       const response = await fetch(
         "https://graph.instagram.com/v21.0/" +
           INSTAGRAM_APP_ID +
