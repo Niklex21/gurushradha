f7df4af93f2612ea0cc5186123057234dfa626cd
Alex Nikanov <alekseynikanov.21@gmail.com>
2024-11-11 20:38:00 -0800

    Added a mobile navbar

--- src/components/MenuLink.tsx
+++ src/components/MenuLink.tsx
@@ -7,7 +7,7 @@ export function MenuLink({ item, pathname }: { item: MenuItemStoryblok, pathname
     const active = href === pathname.slice(1); // to remove the slash
 
     return (
-        <div className="flex relative group/menu">
+        <div className="flex flex-col sm:flex-row relative group/menu">
             <a
                 href={href}
                 className={twMerge(
@@ -18,7 +18,7 @@ export function MenuLink({ item, pathname }: { item: MenuItemStoryblok, pathname
                 {item.name}
             </a>
             {(item.submenus && item.submenus.length > 0) && (
-                <div className="absolute top-0 left-0 flex-col p-2 mt-6 gap-2 -ml-2 rounded-xl bg-white hidden group-hover/menu:flex">
+                <div className="text-nowrap flex sm:absolute top-0 left-0 flex-col p-2 ml-2 sm:mt-6 gap-2 sm:-ml-2 rounded-xl bg-white sm:hidden group-hover/menu:flex">
                     {item.submenus.map((submenu, index) => (
                         <MenuLink item={submenu} pathname={pathname} key={index} />
                     ))}
